902bf8d17064aabecc596889757e6e56a088525f
Shangyuan Han <hanshang@smestaens24ab.sme.utc>
2022-10-05 14:29:21 +0200

    fix html, remove web componants

--- js/main.js
+++ js/main.js
@@ -1,16 +1,4 @@
 "use strict";
-
-customElements.define('menu-icon',
-    class extends HTMLElement {
-        constructor() {
-            super();
-            let template = document.getElementById('custom-menu-icon');
-            let templateContent = template.content.cloneNode(true);
-
-            this.appendChild(templateContent);
-        }
-    })
-
 const menuList = document.querySelectorAll('aside ul li a')
 menuList.forEach(this_element => {
     this_element.addEventListener('click', event => {
