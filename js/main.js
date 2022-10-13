211964bd6b42ee9b05a1a906db382741b9d20dab
Shangyuan Han <shangyuan.han@etu.utc.fr>
2022-10-13 09:54:52 +0200

    move aside to header, add drop down, simplify css

--- js/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const menuList = document.querySelectorAll('aside ul li a')
-menuList.forEach(this_element => {
-    this_element.addEventListener('click', event => {
-        menuList.forEach(element => {element.parentElement.className = ''})
-        this_element.parentElement.className = 'active';
-    })
-})
